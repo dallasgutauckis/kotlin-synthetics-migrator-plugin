9c9f8b1d981b2478bd9597b8db9832dbc5e45be4
GitHub Action <action@github.com>
2020-11-04 01:59:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dallasgutauckis/kotlinsyntheticsmigratorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dallasgutauckis.kotlinsyntheticsmigratorplugin.services
+
+import com.github.dallasgutauckis.kotlinsyntheticsmigratorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dallasgutauckis/kotlinsyntheticsmigratorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dallasgutauckis/kotlinsyntheticsmigratorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dallasgutauckis.kotlinsyntheticsmigratorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dallasgutauckis.kotlinsyntheticsmigratorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
